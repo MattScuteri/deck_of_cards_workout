2c6ecc00274469d793f0ce7dfe96a0f02f106830
MattScuteri <mttscuteri@gmail.com>
2020-04-14 22:23:39 -0400

    while loop to check for pulled_cards size

--- main.cpp
+++ main.cpp
@@ -74,58 +74,59 @@ int main() {
     // std::cin >> diamonds;
     
     srand(time(0));
-
     Deck new_card;
     
-    new_card.build_card();
+    while(new_card.pulled_cards.size() < 52) {
+        new_card.build_card();
 
-    switch(new_card.suit) {
-        case 1:
-            suit_out = "Spades";
-            break;
-        case 2:
-            suit_out = "Clubs";
-            break;
-        case 3:
-            suit_out = "Hearts";
-            break;
-        case 4:
-            suit_out = "Diamonds";
-            break;
-        default:
-            suit_out = "error!";
-    }
+        switch(new_card.suit) {
+            case 1:
+                suit_out = "Spades";
+                break;
+            case 2:
+                suit_out = "Clubs";
+                break;
+            case 3:
+                suit_out = "Hearts";
+                break;
+            case 4:
+                suit_out = "Diamonds";
+                break;
+            default:
+                suit_out = "error!";
+        }
 
-    switch(new_card.num) {
-        case 11:
-            face_card = "Jack";
-            break;
-        case 12:
-            face_card = "Queen";
-            break;
-        case 13:
-            face_card = "King";
-            break;
-        case 14:
-            face_card = "Ace";
-            break;
-        default:
-            face_card = "error!";
-    }
+        switch(new_card.num) {
+            case 11:
+                face_card = "Jack";
+                break;
+            case 12:
+                face_card = "Queen";
+                break;
+            case 13:
+                face_card = "King";
+                break;
+            case 14:
+                face_card = "Ace";
+                break;
+            default:
+                face_card = "error!";
+        }
 
-    if(new_card.num > 10) {
-        std::cout << face_card << " of " << suit_out << "\n";
-    } else {
-        std::cout << new_card.num << " of " << suit_out << "\n";
-    }
+        if(new_card.num > 10) {
+            std::cout << face_card << " of " << suit_out << "\n";
+        } else {
+            std::cout << new_card.num << " of " << suit_out << "\n";
+        }
 
-    // this loop checks to see that cards are being inserted into vector - to delete before finishing program
-    for (int i = 0; i < new_card.pulled_cards.size(); i++) {
-		std::cout << new_card.pulled_cards.at(i) << ' ';
-	}
+        // this loop checks to see that cards are being inserted into vector - to delete before finishing program
+        for (int i = 0; i < new_card.pulled_cards.size(); i++) {
+		    std::cout << new_card.pulled_cards.at(i) << ' ';
+	    }
 
         //create empty vector to hold 52 cards || map with key-value pairs
         //use while loop to check that vector stil has less than 52 cards
         //will need to run verification that all cards in vector are unique
-    // return number and name of exercises
+        // return number and name of exercises
+    }
 }
